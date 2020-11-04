486567a9c02881eeb9a24eac14f9c7a8113ee539
Spextr <70405416+Spextr@users.noreply.github.com>
2020-11-04 15:02:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time 
+time.sleep(5)
+for i in range(200):
+    pyautogui.leftClick(500,500)
---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+title Spextr
+color A
+pip install pyautogui
+pip install time
+py main.py
+pause
